a01bdce511eb6c486009ba182603155298f1d961
Mark Anthony Geronimo <mark_geronimo@rocketmail.com>
2021-09-23 05:28:52 +0000

    2.2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,8 @@
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket-prefix = var.prefix
+  version = "2.2.0"
+  bucket_prefix = var.prefix
   acl    = "private"
 
   versioning = {
